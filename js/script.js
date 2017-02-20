d112307050335aaf1367dacd919dc086a823fa89
Sergey Primirenkov <serg_miren@mail.ru>
2017-02-20 14:15:34 +0300

    Добавил файлы проекта

---
+++ js/script.js
@@ -0,0 +1,34 @@
+var navMain = document.querySelector(".main-nav");
+  var navToggle = document.querySelector(".main-nav__toggle");
+  navMain.classList.remove("main-nav--nojs");
+  navToggle.addEventListener("click", function() {
+    if (navMain.classList.contains("main-nav--closed")) {
+      navMain.classList.remove("main-nav--closed");
+      navMain.classList.add("main-nav--opened");
+    } else {
+      navMain.classList.add("main-nav--closed");
+      navMain.classList.remove("main-nav--opened");
+    }
+  });
+
+  var login = document.querySelector(".main-nav__user-login"),
+      login_close = document.querySelector(".login__close"),
+      login_form = document.querySelector(".login"),
+      login_input = document.querySelector(".login__input");
+
+  login.addEventListener("click", function(event) {
+    event.preventDefault();
+    login_form.classList.add("login--open");
+    login_input.focus();
+    document.onkeydown = function(evt) {
+    evt = evt || window.event;
+      if (evt.keyCode == 27) {
+        login_form.classList.remove("login--open");
+      }
+    };
+  });
+
+  login_close.addEventListener("click", function(event) {
+    event.preventDefault();
+    login_form.classList.remove("login--open");
+  });
